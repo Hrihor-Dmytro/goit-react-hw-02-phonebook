1c3dc0cd2df031b30095c7dfe42e31d3210b3c6f
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2022-11-04 17:46:20 +0200

    fix

--- src/components/ContactList/ContactsList.jsx
+++ src/components/ContactList/ContactsList.jsx
@@ -2,7 +2,7 @@ export const ContactList = ({ contactsArr, onDelitContact }) => {
   return contactsArr.map(({ id, name, number }) => (
     <ul>
       <li key={id}>
-        <p> {name} </p>
+        <h2> {name} </h2>
         <p>{number}</p>
         <button onClick={() => onDelitContact(id)}>Delite</button>
       </li>
